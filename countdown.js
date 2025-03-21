70079dd1dac8542c5a1fc3992503135259d164cb
SymLynks <3082841+SymLynks@users.noreply.github.com>
2025-03-21 20:39:25 +0100

    fix timezone

--- countdown.js
+++ countdown.js
@@ -1,23 +1,24 @@
-const countdownDate = new Date('2025-05-10T00:00:00').getTime();
-const countdownElement = document.querySelector('.countdown');
 
 function updateCountdown() {
+    const targetDate = new Date('2025-05-10T00:00:00Z').getTime();
     const now = new Date().getTime();
-    const distance = countdownDate - now;
+    const timeLeft = targetDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    if (timeLeft < 0) {
+        document.querySelector('.countdown').innerHTML = 'Cerby Plush is shipping!';
+        return;
+    }
 
     document.querySelector('.days').innerText = days;
     document.querySelector('.hours').innerText = hours;
     document.querySelector('.minutes').innerText = minutes;
     document.querySelector('.seconds').innerText = seconds;
 
-    if (distance < 0) {
-        document.querySelector('.countdown').innerHTML = 'Die Lieferung ist da!';
-    }
 }
 
 setInterval(updateCountdown, 1000);
